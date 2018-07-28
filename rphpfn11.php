1f1ef18a3cd49630284b83f0ec16454047ec5845
acer one 10 <selusin.info@gmail.com>
2018-07-28 13:56:27 +0700

    1

--- rphpfn11.php
+++ rphpfn11.php
@@ -7612,6 +7612,17 @@ function Database_Connecting(&$info) {
 	// $info["db"] = "production_db";
 	// }
 
+	if (ewr_CurrentUserIP () == "127.0.0.1"  || ewr_CurrentUserIP () == "::1"  || ewr_CurrentHost () == "localhost" ) { // testing on local PC
+		$info["host"] = "localhost";
+		$info["user"] = "root"; // sesuaikan dengan username database di komputer localhost
+		$info["pass"] = "admin"; // sesuaikan dengan password database di komputer localhost
+		$info["db"] = "db_lapkeu"; // sesuaikan dengan nama database di komputer localhost
+	} elseif (ewr_CurrentHost () == "lapkeu.selusin.net") { // setting koneksi database untuk komputer server
+		$info["host"] = "mysql.idhostinger.com";  // sesuaikan dengan ip address atau hostname komputer server
+		$info["user"] = "u433254588_lapke"; // sesuaikan dengan username database di komputer server
+		$info["pass"] = "M457r1P 81"; // sesuaikan deengan password database di komputer server
+		$info["db"] = "u433254588_lapke"; // sesuaikan dengan nama database di komputer server
+	}
 }
 
 // Database Connected event
