438d7d5a4808cd1c3e34cca9f5b24a7c55ce6b94
olapps3998 <olapps3998@gmail.com>
2018-08-10 20:16:00 +0700

    1

--- ewmenu.php
+++ ewmenu.php
@@ -7,8 +7,9 @@ $RootMenu->AddMenuItem(10055, "mci_Keuangan", $Language->MenuPhrase("10055", "Me
 $RootMenu->AddMenuItem(10016, "mi_t08_penerimaan", $Language->MenuPhrase("10016", "MenuText"), "t08_penerimaanlist.php", 10055, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t08_penerimaan'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(6, "mi_t06_pengeluaran", $Language->MenuPhrase("6", "MenuText"), "t06_pengeluaranlist.php", 10055, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t06_pengeluaran'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10088, "mci_Laporan", $Language->MenuPhrase("10088", "MenuText"), "", 10055, "", IsLoggedIn(), FALSE, TRUE, "");
-$RootMenu->AddMenuItem(10019, "mri_r025fterima5fkeluar", $Language->MenuPhrase("10019", "MenuText"), "r02_terima_keluarsmry.php", 10088, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}r02_terima_keluar'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10017, "mri_r025fterima5fkeluar", $Language->MenuPhrase("10017", "MenuText"), "r02_terima_keluarsmry.php", 10088, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}r02_terima_keluar'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10089, "mci_Excel", $Language->MenuPhrase("10089", "MenuText"), "r03_terima_keluar.php", 10088, "", IsLoggedIn(), FALSE, TRUE, "");
+$RootMenu->AddMenuItem(10120, "mi_r04_terima_keluar_php", $Language->MenuPhrase("10120", "MenuText"), "r04_terima_keluar.php", 10088, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}r04_terima_keluar.php'), FALSE, TRUE, "");
 $RootMenu->AddMenuItem(22, "mci_Setup", $Language->MenuPhrase("22", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE, "");
 $RootMenu->AddMenuItem(33, "mi_t07_sekolah", $Language->MenuPhrase("33", "MenuText"), "t07_sekolahlist.php", 22, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t07_sekolah'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(1, "mi_t01_supplier", $Language->MenuPhrase("1", "MenuText"), "t01_supplierlist.php", 22, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t01_supplier'), FALSE, FALSE, "");
@@ -19,12 +20,12 @@ $RootMenu->AddMenuItem(32, "mci_User", $Language->MenuPhrase("32", "MenuText"), 
 $RootMenu->AddMenuItem(7, "mi_t96_employees", $Language->MenuPhrase("7", "MenuText"), "t96_employeeslist.php", 32, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t96_employees'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(8, "mi_t97_userlevels", $Language->MenuPhrase("8", "MenuText"), "t97_userlevelslist.php", 32, "", IsAdmin(), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(23, "mi_t99_audittrail", $Language->MenuPhrase("23", "MenuText"), "t99_audittraillist.php", 32, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t99_audittrail'), FALSE, FALSE, "");
-$RootMenu->AddMenuItem(10012, "mci_List", $Language->MenuPhrase("10012", "MenuText"), "", -1, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", IsLoggedIn(), FALSE, TRUE, "");
-$RootMenu->AddMenuItem(10007, "mri_t075fsekolah", $Language->MenuPhrase("10007", "MenuText"), "t07_sekolahrpt.php", 10012, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t07_sekolah'), FALSE, FALSE, "");
-$RootMenu->AddMenuItem(10001, "mri_t015fsupplier", $Language->MenuPhrase("10001", "MenuText"), "t01_supplierrpt.php", 10012, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t01_supplier'), FALSE, FALSE, "");
-$RootMenu->AddMenuItem(10002, "mri_t025fsatuan", $Language->MenuPhrase("10002", "MenuText"), "t02_satuanrpt.php", 10012, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t02_satuan'), FALSE, FALSE, "");
-$RootMenu->AddMenuItem(10003, "mri_t035fbarang", $Language->MenuPhrase("10003", "MenuText"), "t03_barangrpt.php", 10012, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t03_barang'), FALSE, FALSE, "");
-$RootMenu->AddMenuItem(10014, "mri_r015fjenis5fpengeluaran", $Language->MenuPhrase("10014", "MenuText"), "r01_jenis_pengeluaransmry.php", 10012, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}r01_jenis_pengeluaran'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10118, "mci_List", $Language->MenuPhrase("10118", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE, "");
+$RootMenu->AddMenuItem(10007, "mri_t075fsekolah", $Language->MenuPhrase("10007", "MenuText"), "t07_sekolahrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t07_sekolah'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10001, "mri_t015fsupplier", $Language->MenuPhrase("10001", "MenuText"), "t01_supplierrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t01_supplier'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10002, "mri_t025fsatuan", $Language->MenuPhrase("10002", "MenuText"), "t02_satuanrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t02_satuan'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10003, "mri_t035fbarang", $Language->MenuPhrase("10003", "MenuText"), "t03_barangrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t03_barang'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10014, "mri_r015fjenis5fpengeluaran", $Language->MenuPhrase("10014", "MenuText"), "r01_jenis_pengeluaransmry.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}r01_jenis_pengeluaran'), FALSE, FALSE, "");
 echo $RootMenu->ToScript();
 ?>
 <div class="ewVertical" id="ewMenu"></div>
---
+++ unused/r04_terima_keluar.php
@@ -0,0 +1,345 @@
+<?php
+if (session_id() == "") session_start(); // Initialize Session data
+ob_start();
+?>
+<?php include_once "rcfg11.php" ?>
+<?php $EWR_ROOT_RELATIVE_PATH = ""; ?>
+<?php include_once ((EW_USE_ADODB) ? "adodb5/adodb.inc.php" : "phprptinc/ewmysql.php") ?>
+<?php include_once "rphpfn11.php" ?>
+<?php include_once "rusrfn11.php" ?>
+<?php
+
+//
+// Page class
+//
+
+$r04_terima_keluar_php = NULL; // Initialize page object first
+
+class crr04_terima_keluar_php {
+
+	// Page ID
+	var $PageID = 'custom';
+
+	// Project ID
+	var $ProjectID = "{3CDC6268-D928-4495-B72A-CA5D35EAE344}";
+
+	// Page object name
+	var $PageObjName = 'r04_terima_keluar_php';
+
+	// Page headings
+	var $Heading = '';
+	var $Subheading = '';
+
+	// Page heading
+	function PageHeading() {
+		global $ReportLanguage;
+		if ($this->Heading <> "")
+			return $this->Heading;
+		if (method_exists($this, "TableCaption"))
+			return $this->TableCaption();
+		return "";
+	}
+
+	// Page subheading
+	function PageSubheading() {
+		global $ReportLanguage;
+		if ($this->Subheading <> "")
+			return $this->Subheading;
+		return "";
+	}
+
+	// Page name
+	function PageName() {
+		return ewr_CurrentPage();
+	}
+
+	// Page URL
+	function PageUrl() {
+		$PageUrl = ewr_CurrentPage() . "?";
+		return $PageUrl;
+	}
+
+	// Message
+	function getMessage() {
+		return @$_SESSION[EWR_SESSION_MESSAGE];
+	}
+
+	function setMessage($v) {
+		ewr_AddMessage($_SESSION[EWR_SESSION_MESSAGE], $v);
+	}
+
+	function getFailureMessage() {
+		return @$_SESSION[EWR_SESSION_FAILURE_MESSAGE];
+	}
+
+	function setFailureMessage($v) {
+		ewr_AddMessage($_SESSION[EWR_SESSION_FAILURE_MESSAGE], $v);
+	}
+
+	function getSuccessMessage() {
+		return @$_SESSION[EWR_SESSION_SUCCESS_MESSAGE];
+	}
+
+	function setSuccessMessage($v) {
+		ewr_AddMessage($_SESSION[EWR_SESSION_SUCCESS_MESSAGE], $v);
+	}
+
+	function getWarningMessage() {
+		return @$_SESSION[EWR_SESSION_WARNING_MESSAGE];
+	}
+
+	function setWarningMessage($v) {
+		ewr_AddMessage($_SESSION[EWR_SESSION_WARNING_MESSAGE], $v);
+	}
+
+		// Show message
+	function ShowMessage() {
+		$hidden = FALSE;
+		$html = "";
+
+		// Message
+		$sMessage = $this->getMessage();
+		$this->Message_Showing($sMessage, "");
+		if ($sMessage <> "") { // Message in Session, display
+			if (!$hidden)
+				$sMessage = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>" . $sMessage;
+			$html .= "<div class=\"alert alert-info ewInfo\">" . $sMessage . "</div>";
+			$_SESSION[EWR_SESSION_MESSAGE] = ""; // Clear message in Session
+		}
+
+		// Warning message
+		$sWarningMessage = $this->getWarningMessage();
+		$this->Message_Showing($sWarningMessage, "warning");
+		if ($sWarningMessage <> "") { // Message in Session, display
+			if (!$hidden)
+				$sWarningMessage = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>" . $sWarningMessage;
+			$html .= "<div class=\"alert alert-warning ewWarning\">" . $sWarningMessage . "</div>";
+			$_SESSION[EWR_SESSION_WARNING_MESSAGE] = ""; // Clear message in Session
+		}
+
+		// Success message
+		$sSuccessMessage = $this->getSuccessMessage();
+		$this->Message_Showing($sSuccessMessage, "success");
+		if ($sSuccessMessage <> "") { // Message in Session, display
+			if (!$hidden)
+				$sSuccessMessage = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>" . $sSuccessMessage;
+			$html .= "<div class=\"alert alert-success ewSuccess\">" . $sSuccessMessage . "</div>";
+			$_SESSION[EWR_SESSION_SUCCESS_MESSAGE] = ""; // Clear message in Session
+		}
+
+		// Failure message
+		$sErrorMessage = $this->getFailureMessage();
+		$this->Message_Showing($sErrorMessage, "failure");
+		if ($sErrorMessage <> "") { // Message in Session, display
+			if (!$hidden)
+				$sErrorMessage = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>" . $sErrorMessage;
+			$html .= "<div class=\"alert alert-danger ewError\">" . $sErrorMessage . "</div>";
+			$_SESSION[EWR_SESSION_FAILURE_MESSAGE] = ""; // Clear message in Session
+		}
+		echo "<div class=\"ewMessageDialog\"" . (($hidden) ? " style=\"display: none;\"" : "") . ">" . $html . "</div>";
+	}
+
+	// Validate page request
+	function IsPageRequest() {
+		return TRUE;
+	}
+	var $Token = "";
+	var $CheckToken = EWR_CHECK_TOKEN;
+	var $CheckTokenFn = "ewr_CheckToken";
+	var $CreateTokenFn = "ewr_CreateToken";
+
+	// Valid Post
+	function ValidPost() {
+		if (!$this->CheckToken || !ewr_IsHttpPost())
+			return TRUE;
+		if (!isset($_POST[EWR_TOKEN_NAME]))
+			return FALSE;
+		$fn = $this->CheckTokenFn;
+		if (is_callable($fn))
+			return $fn($_POST[EWR_TOKEN_NAME]);
+		return FALSE;
+	}
+
+	// Create Token
+	function CreateToken() {
+		global $grToken;
+		if ($this->CheckToken) {
+			$fn = $this->CreateTokenFn;
+			if ($this->Token == "" && is_callable($fn)) // Create token
+				$this->Token = $fn();
+			$grToken = $this->Token; // Save to global variable
+		}
+	}
+
+	//
+	// Page class constructor
+	//
+	function __construct() {
+		global $conn, $ReportLanguage;
+		global $UserTable, $UserTableConn;
+
+		// Language object
+		$ReportLanguage = new crLanguage();
+
+		// Page ID
+		if (!defined("EWR_PAGE_ID"))
+			define("EWR_PAGE_ID", 'custom', TRUE);
+
+		// Table name (for backward compatibility)
+		if (!defined("EWR_TABLE_NAME"))
+			define("EWR_TABLE_NAME", 'r04_terima_keluar.php', TRUE);
+
+		// Start timer
+		if (!isset($GLOBALS["grTimer"]))
+			$GLOBALS["grTimer"] = new crTimer();
+
+		// Debug message
+		ewr_LoadDebugMsg();
+
+		// Open connection
+		if (!isset($conn)) $conn = ewr_Connect();
+
+		// User table object (t96_employees)
+		if (!isset($UserTable)) {
+			$UserTable = new crt96_employees();
+			$UserTableConn = ReportConn($UserTable->DBID);
+		}
+	}
+
+	//
+	// Page_Init
+	//
+	function Page_Init() {
+		global $gsExport, $gsExportFile, $gsEmailContentType, $ReportLanguage, $Security, $UserProfile;
+		global $gsCustomExport;
+
+		// User profile
+		$UserProfile = new crUserProfile();
+
+		// Security
+		$Security = new crAdvancedSecurity();
+		if (!$Security->IsLoggedIn()) $Security->AutoLogin(); // Auto login
+		$Security->TablePermission_Loading();
+		$Security->LoadCurrentUserLevel($this->ProjectID . 'r04_terima_keluar.php');
+		$Security->TablePermission_Loaded();
+		if (!$Security->CanList()) {
+			$Security->SaveLastUrl();
+			$this->setFailureMessage(ewr_DeniedMsg()); // Set no permission
+			$this->Page_Terminate(ewr_GetUrl("index.php"));
+		}
+		$Security->UserID_Loading();
+		if ($Security->IsLoggedIn()) $Security->LoadUserID();
+		$Security->UserID_Loaded();
+		if ($Security->IsLoggedIn() && strval($Security->CurrentUserID()) == "") {
+			$Security->SaveLastUrl();
+			$this->setFailureMessage(ewr_DeniedMsg()); // Set no permission
+			$this->Page_Terminate(ewr_GetUrl("login.php"));
+		}
+
+		// Global Page Loading event (in userfn*.php)
+		Page_Loading();
+
+		// Check token
+		if (!$this->ValidPost()) {
+			echo $ReportLanguage->Phrase("InvalidPostRequest");
+			$this->Page_Terminate();
+			exit();
+		}
+
+		// Create Token
+		$this->CreateToken();
+	}
+
+	//
+	// Page_Terminate
+	//
+	function Page_Terminate($url = "") {
+		global $ReportLanguage, $EWR_EXPORT, $gsExportFile;
+		global $grDashboardReport;
+
+		// Global Page Unloaded event (in userfn*.php)
+		Page_Unloaded();
+
+		// Close connection
+		ewr_CloseConn();
+
+		// Go to URL if specified
+		if ($url <> "") {
+			if (!EWR_DEBUG_ENABLED && ob_get_length())
+				ob_end_clean();
+			ewr_SaveDebugMsg();
+			header("Location: " . $url);
+		}
+		exit();
+	}
+
+	//
+	// Page main
+	//
+	function Page_Main() {
+
+		// Set up Breadcrumb
+		$this->SetupBreadcrumb();
+	}
+
+	// Set up Breadcrumb
+	function SetupBreadcrumb() {
+		global $ReportBreadcrumb, $ReportLanguage;
+		$ReportBreadcrumb = new crBreadcrumb();
+		$url = substr(ewr_CurrentUrl(), strrpos(ewr_CurrentUrl(), "/")+1);
+		$ReportBreadcrumb->Add("custom", "r04_terima_keluar_php", $url, "", "r04_terima_keluar_php", TRUE);
+		$this->Heading = $ReportLanguage->TablePhrase("r04_terima_keluar_php", "TblCaption"); 
+	}
+
+	// Message Showing event
+	// $type = ''|'success'|'failure'|'warning'
+	function Message_Showing(&$msg, $type) {
+		if ($type == 'success') {
+
+			//$msg = "your success message";
+		} elseif ($type == 'failure') {
+
+			//$msg = "your failure message";
+		} elseif ($type == 'warning') {
+
+			//$msg = "your warning message";
+		} else {
+
+			//$msg = "your message";
+		}
+	}
+}
+?>
+<?php
+ewr_Header(FALSE);
+
+// Create page object
+if (!isset($r04_terima_keluar_php)) $r04_terima_keluar_php = new crr04_terima_keluar_php();
+if (isset($Page)) $OldPage = $Page;
+$Page = &$r04_terima_keluar_php;
+
+// Page init
+$Page->Page_Init();
+
+// Page main
+$Page->Page_Main();
+
+// Global Page Rendering event (in ewrusrfn*.php)
+Page_Rendering();
+?>
+<?php include_once "phprptinc/header.php" ?>
+<!-- div class="ewToolbar">
+<div class="clearfix"></div>
+</div -->
+<div class="panel panel-default">
+	<div class="panel-heading">Latest News</div>
+	<div class="panel-body">
+		<p>Laporan Keuangan. @2018 Selaras Solusindo. All rights reserved.</p>
+	</div>
+</div>
+<?php if (EWR_DEBUG_ENABLED) echo ewr_DebugMsg(); ?>
+<?php include_once "phprptinc/footer.php" ?>
+<?php
+$Page->Page_Terminate();
+if (isset($OldPage)) $Page = $OldPage;
+?>
