89f9877635e3eef7a03ac6329db74571e54fa6f3
dell latitude e6420 <apps.selusin@gmail.com>
2018-08-19 19:39:49 +0700

    1

--- ewcfg14.php
+++ ewcfg14.php
@@ -25,7 +25,7 @@ define("EW_CONFIG_FILE_FOLDER", EW_PROJECT_NAME, TRUE); // Config file name
 define("EW_PROJECT_ID", "{239A2A32-109A-412F-A3CB-FF6290C167FC}", TRUE); // Project ID (GUID)
 $EW_RELATED_PROJECT_ID = "";
 $EW_RELATED_LANGUAGE_FOLDER = "";
-define("EW_RANDOM_KEY", 'i9hoFG813ZSxifpj', TRUE); // Random key for encryption
+define("EW_RANDOM_KEY", 'WCA82qQ7kmrwQ3rL', TRUE); // Random key for encryption
 define("EW_PROJECT_STYLESHEET_FILENAME", "phpcss/prj_lapkeu.css", TRUE); // Project stylesheet file name
 define("EW_CHARSET", "utf-8", TRUE); // Project charset
 define("EW_EMAIL_CHARSET", EW_CHARSET, TRUE); // Email charset
