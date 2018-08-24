9762495ae3b1615b1428e04612a1f98426e1d70c
dell latitude e6420 <apps.selusin@gmail.com>
2018-08-25 02:11:27 +0700

    1

--- index.php
+++ index.php
@@ -337,6 +337,8 @@ class cdefault {
 			$this->Page_Terminate("t98_userlevelpermissionslist.php");
 		if ($Security->AllowList(CurrentProjectID() . 't99_audittrail'))
 			$this->Page_Terminate("t99_audittraillist.php");
+		if ($Security->AllowList(CurrentProjectID() . 'r05_keuanganold.php'))
+			$this->Page_Terminate("r05_keuanganold.php");
 		if ($Security->IsLoggedIn()) {
 			$this->setFailureMessage(ew_DeniedMsg() . "<br><br><a href=\"logout.php\">" . $Language->Phrase("BackToLogin") . "</a>");
 		} else {
