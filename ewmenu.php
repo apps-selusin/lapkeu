5d8e9dcd5cbbc27dad1d04494707be6f768ff727
Presario CQ43 <apps.selusin@gmail.com>
2018-08-30 03:07:56 +0700

    .

--- ewmenu.php
+++ ewmenu.php
@@ -29,8 +29,6 @@ $RootMenu->AddMenuItem(32, "mci_User", $Language->MenuPhrase("32", "MenuText"), 
 $RootMenu->AddMenuItem(7, "mi_t96_employees", $Language->MenuPhrase("7", "MenuText"), "t96_employeeslist.php", 32, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t96_employees'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(8, "mi_t97_userlevels", $Language->MenuPhrase("8", "MenuText"), "t97_userlevelslist.php", 32, "", IsAdmin(), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(23, "mi_t99_audittrail", $Language->MenuPhrase("23", "MenuText"), "t99_audittraillist.php", 32, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}t99_audittrail'), FALSE, FALSE, "");
-$RootMenu->AddMenuItem(10176, "mi_cf04_update_php", $Language->MenuPhrase("10176", "MenuText"), "cf04_update.php", 22, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}cf04_update.php'), FALSE, TRUE, "");
-$RootMenu->AddMenuItem(10177, "mi_cf05_backup_php", $Language->MenuPhrase("10177", "MenuText"), "cf05_backup.php", 22, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}cf05_backup.php'), FALSE, TRUE, "");
 $RootMenu->AddMenuItem(10118, "mci_List", $Language->MenuPhrase("10118", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE, "");
 $RootMenu->AddMenuItem(10007, "mri_t075fsekolah", $Language->MenuPhrase("10007", "MenuText"), "t07_sekolahrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t07_sekolah'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10001, "mri_t015fsupplier", $Language->MenuPhrase("10001", "MenuText"), "t01_supplierrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t01_supplier'), FALSE, FALSE, "");
@@ -38,6 +36,9 @@ $RootMenu->AddMenuItem(10002, "mri_t025fsatuan", $Language->MenuPhrase("10002", 
 $RootMenu->AddMenuItem(10003, "mri_t035fbarang", $Language->MenuPhrase("10003", "MenuText"), "t03_barangrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t03_barang'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10014, "mri_r015fjenis5fpengeluaran", $Language->MenuPhrase("10014", "MenuText"), "r01_jenis_pengeluaransmry.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}r01_jenis_pengeluaran'), FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10011, "mri_t995faudittrail", $Language->MenuPhrase("10011", "MenuText"), "t99_audittrailrpt.php", 10118, "{3CDC6268-D928-4495-B72A-CA5D35EAE344}", AllowListMenu('{3CDC6268-D928-4495-B72A-CA5D35EAE344}t99_audittrail'), FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10240, "mci_Tools", $Language->MenuPhrase("10240", "MenuText"), "", -1, "", IsLoggedIn(), FALSE, TRUE, "");
+$RootMenu->AddMenuItem(10176, "mi_cf04_update_php", $Language->MenuPhrase("10176", "MenuText"), "cf04_update.php", 10240, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}cf04_update.php'), FALSE, TRUE, "");
+$RootMenu->AddMenuItem(10177, "mi_cf05_backup_php", $Language->MenuPhrase("10177", "MenuText"), "cf05_backup.php", 10240, "", AllowListMenu('{239A2A32-109A-412F-A3CB-FF6290C167FC}cf05_backup.php'), FALSE, TRUE, "");
 echo $RootMenu->ToScript();
 ?>
 <div class="ewVertical" id="ewMenu"></div>
