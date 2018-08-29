c1b9c8b6b298601edec426e17bbb418c9ba9671b
Presario CQ43 <apps.selusin@gmail.com>
2018-08-29 20:17:25 +0700

    0

--- logout.php
+++ logout.php
@@ -363,62 +363,23 @@ class clogout {
 
 		// Example:
 		//$url = "your URL";
+		// backup database
 
-		backup_tables('localhost','root','admin','db_lapkeu');
-
-	/* backup the db OR just a table */
-
-	function backup_tables($host,$user,$pass,$name,$tables = '*')
-	{
-		$link = mysql_connect($host,$user,$pass);
-		mysql_select_db($name,$link);
-
-		//get all of the tables
-		if($tables == '*')
-		{
-			$tables = array();
-			$result = mysql_query('SHOW TABLES');
-			while($row = mysql_fetch_row($result))
-			{
-				$tables[] = $row[0];
-			}
-		}
-		else
-		{
-			$tables = is_array($tables) ? $tables : explode(',',$tables);
+		$info = array();
+		if (ew_CurrentUserIP () == "127.0.0.1"  || ew_CurrentUserIP () == ":: 1"  || ew_CurrentHost () == "localhost" ) { // testing on local PC
+			$info["host"] = "localhost";
+			$info["user"] = "root"; // sesuaikan dengan username database di komputer localhost
+			$info["pass"] = "admin"; // sesuaikan dengan password database di komputer localhost
+			$info["db"] = "db_lapkeu"; // sesuaikan dengan nama database di komputer localhost
+		} elseif (ew_CurrentHost () == "lapkeu.selusin.net") { // setting koneksi database untuk komputer server
+			$info["host"] = "mysql.hostinger.co.id";  // sesuaikan dengan ip address atau hostname komputer server
+			$info["user"] = "u433254588_lapke"; // sesuaikan dengan username database di komputer server
+			$info["pass"] = "M457r1P 81"; // sesuaikan deengan password database di komputer server
+			$info["db"] = "u433254588_lapke"; // sesuaikan dengan nama database di komputer server
 		}
 
-		//cycle through
-		foreach($tables as $table)
-		{
-			$result = mysql_query('SELECT * FROM '.$table);
-			$num_fields = mysql_num_fields($result);
-			$return.= 'DROP TABLE '.$table.';';
-			$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
-			$return.= "\n\n".$row2[1].";\n\n";
-			for ($i = 0; $i < $num_fields; $i++) 
-			{
-				while($row = mysql_fetch_row($result))
-				{
-					$return.= 'INSERT INTO '.$table.' VALUES(';
-					for($j=0; $j < $num_fields; $j++) 
-					{
-						$row[$j] = addslashes($row[$j]);
-						$row[$j] = ereg_replace("\n","\\n",$row[$j]);
-						if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
-						if ($j < ($num_fields-1)) { $return.= ','; }
-					}
-					$return.= ");\n";
-				}
-			}
-			$return.="\n\n\n";
-		}
-
-		//save file
-		$handle = fopen('db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql','w+');
-		fwrite($handle,$return);
-		fclose($handle);
-	}
+		//backup_tables('localhost','root','admin','db_lapkeu');
+		backup_tables($info["host"], $info["user"], $info["pass"], $info["db"]);
 		$url = ".";
 	}
 
